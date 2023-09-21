0c2b085c17e544926d849e4d478f7669afaa6194
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-09-21 13:52:21 +0900

    Optional

---
+++ src/sec01_08/with_optional/WithOptionalExampleMain1.java
@@ -0,0 +1,17 @@
+package sec01_08.with_optional;
+
+import java.util.Optional;
+
+public class WithOptionalExampleMain1 {
+    public static void main(String[] args) {
+        MapRepository mapRepository = new MapRepository();
+        Optional<String> string = mapRepository.getOptionalValue("NOT_EXIST_KEY");
+
+        string.ifPresentOrElse(
+                str -> System.out.println(str.toUpperCase()),
+                () -> {
+                    throw new RuntimeException("키가 존재하지 않습니다.");
+                }
+        );
+    }
+}
