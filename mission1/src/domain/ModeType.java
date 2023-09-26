413a51460834071a8a1ee4c712c2d940cfe12c9d
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-09-26 12:20:21 +0900

    mode service 합치기

--- mission1/src/domain/ModeType.java
+++ mission1/src/domain/ModeType.java
@@ -1,5 +1,23 @@
 package domain;
 
+import service.Mode;
+
+import java.io.IOException;
+import java.util.function.BiFunction;
+
 public enum ModeType {
-    NORMAL, TEST;
+    NORMAL((Runnable)() -> {
+        System.out.println("[System] 일반 모드로 애플리케이션을 실행합니다.");
+
+    }),
+    TEST((Runnable)() -> {
+        System.out.println("[System] 테스트 모드로 애플리케이션을 실행합니다.");
+        return new Mode("test");
+    });
+
+    ModeType(Mode expression) {
+        this.expression = expression;
+    }
+
+    private Mode expression;
 }
