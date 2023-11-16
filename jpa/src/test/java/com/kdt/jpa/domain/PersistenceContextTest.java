a3d963d99e1566e82a5336d4244ef0cf8019c9ff
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-11-17 03:57:48 +0900

    영속성 컨텍스트 테스트

--- jpa/src/test/java/com/kdt/jpa/domain/PersistenceContextTest.java
+++ jpa/src/test/java/com/kdt/jpa/domain/PersistenceContextTest.java
@@ -50,5 +50,71 @@ public class PersistenceContextTest {
         transaction.commit();
 
         entityManager.detach(customer);
+
+        Customer selected = entityManager.find(Customer.class, 1L);
+        log.info("{} {}", selected.getFirstName(), selected.getLastName());
+    }
+
+    @Test
+    void 조회_1차캐시_이용() {
+        EntityManager entityManager = emf.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        transaction.begin();
+
+        Customer customer = new Customer();
+        customer.setId(1L);
+        customer.setFirstName("honggu");
+        customer.setLastName("kang");
+
+        entityManager.persist(customer);
+        transaction.commit();
+
+        Customer selected = entityManager.find(Customer.class, 1L);
+        log.info("{} {}", selected.getFirstName(), selected.getLastName());
+    }
+
+    @Test
+    void 수정() {
+        EntityManager entityManager = emf.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        transaction.begin();
+
+        Customer customer = new Customer();
+        customer.setId(1L);
+        customer.setFirstName("honggu");
+        customer.setLastName("kang");
+
+        entityManager.persist(customer);
+        transaction.commit();
+
+        transaction.begin();
+        customer.setFirstName("guppy");
+        customer.setLastName("hong");
+
+        transaction.commit();
+    }
+
+    @Test
+    void 삭제() {
+        EntityManager entityManager = emf.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        transaction.begin();
+
+        Customer customer = new Customer();
+        customer.setId(1L);
+        customer.setFirstName("honggu");
+        customer.setLastName("kang");
+
+        entityManager.persist(customer);
+        transaction.commit();
+
+        transaction.begin();
+
+        entityManager.remove(customer);
+
+        transaction.commit();
     }
 }
